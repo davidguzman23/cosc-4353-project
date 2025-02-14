7985c3d5931b7f5f89a16ca4b34d773dff3ffeef
davidguzman23 <151577458+davidguzman23@users.noreply.github.com>
2025-02-13 18:16:36 -0600

    Add react-app/

---
+++ react-app/src/LoginPage.tsx
@@ -0,0 +1,14 @@
+import { Container, Paper } from "@mui/material";
+
+const LoginPage = () => {
+    return (
+        <Container maxWidth="xs">
+            <Paper elevation={10} sx={{marginTop: 8, padding: 2}}>
+            Login Page
+            </Paper>
+
+        </Container>
+    );
+};
+
+export default LoginPage;
